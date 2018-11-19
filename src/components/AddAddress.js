721b0e0fd8ec163cd792e40ec258f46c41cb9ca8
Ignat Romanov <romign@gmail.com>
2018-11-19 20:02:26 +0300

    add address checker

--- src/components/AddAddress.js
+++ src/components/AddAddress.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Button, Form, FormGroup, Input, Label} from 'reactstrap'
 import card from './../decorators/card'
+import {isAddress} from "../ethereum/isAddress";
+import {connect} from 'react-redux'
 
 class AddAddress extends Component {
   static defaultProps = {};
@@ -17,12 +19,13 @@ class AddAddress extends Component {
   }
   
   render() {
+    const {address} = this.state
     return (
       <Form inline>
         <FormGroup className="mb-5 mr-sm-5 mb-sm-0">
           <Label for="watchedAddress" className="mr-sm-5">Address to watch</Label>
-          <Input type="text" name="address" id="watchedAddress" placeholder='0x0' value={this.state.address}
-                 className="mr-sm-5" onChange={this.addressChangeHandler}/>
+          <Input type="text" name="address" id="watchedAddress" placeholder='0x0' value={address}
+                 valid={isAddress(address)} className="mr-sm-5" onChange={this.addressChangeHandler}/>
         </FormGroup>
         <Button onClick={this.addAddress}>Add address</Button>
       </Form>
@@ -31,3 +34,4 @@ class AddAddress extends Component {
 }
 
 export default card('Add new address to watch', AddAddress);
+
